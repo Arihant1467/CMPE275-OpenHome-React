51db6f3c2d83086e02867e179a0c68e75f2edaa9
Arihant1467 <arihant95@gmail.com>
2019-12-16 14:57:20 -0800

    printing env variables

--- src/components/constants.js
+++ src/components/constants.js
@@ -1,4 +1,10 @@
 
+
+console.log(process.env.JAVA_BACKEND);
+console.log(process.env.HEROKU_URL);
+console.log(process.env.IMAGE_CDN);
+console.log(process.env.API_KEY);
+
 export const BASE_URL = process.env.JAVA_BACKEND==null ? "http://localhost:8080/OpenHome_war/api": process.env.JAVA_BACKEND;
 export const FRONTEND_URL = process.env.HEROKU_URL==null ? "http://localhost:3000/": process.env.HEROKU_URL;
 export const IMG_RETRIEVE_BASE_URL = process.env.IMAGE_CDN==null ?  "http://localhost:3500": process.env.IMAGE_CDN;
