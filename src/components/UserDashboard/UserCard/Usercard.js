32d3d086d76c951b5931754359d70a1fdbbd3563
deepikay912 <deepika640yannamani@gmail.com>
2019-12-17 13:07:34 -0800

    disable check in ceckout buttons

--- src/components/UserDashboard/UserCard/Usercard.js
+++ src/components/UserDashboard/UserCard/Usercard.js
@@ -10,13 +10,59 @@ class UserCard extends Component {
 
     constructor(props){
         super(props);
-      //  this.checkIn.bind(this);
-       // this.checkOut.bind(this);
-
+      const trip = this.props.data; 
+  
        this.state = {
         rating: 5,
-        review : ""
+        review : "",
+        checkInState : false,
+        checkOutState : false,
+        cancelled : false,
+        reviews : false,
+        status : ""
     }
+
+    }
+
+
+    componentDidMount() {
+
+      console.log("in component did mount");
+
+      const trip = this.props.data; 
+
+      console.log("trip data" + trip.checkIn);
+
+      this.setState({
+        checkInState :  trip.checkIn != null || trip.isCancelled
+      } );
+
+      this.setState({
+        checkOutState :  trip.checkOut != null || trip.isCancelled
+      } );
+
+      this.setState({
+        cancelled : trip.isCancelled || ( trip.checkIn != null  &&  trip.checkOut != null)
+      } );
+
+
+      this.setState({
+        reviews : trip.reservationReview != null
+      } );
+
+      let temp ="";
+      if(trip.isCancelled)
+        temp = "Reservation is cancelled"
+      else if(trip.checkIn == null )
+        temp = "Reserved"
+      else if(trip.checkOut == null)
+        temp = "Checkin complete"
+      else
+        temp = "Checkout complete"
+
+      this.setState({
+        status : temp
+      } );
 
     }
 
@@ -34,6 +80,7 @@ class UserCard extends Component {
         "bookingId" : trip.bookingId
     }
 
+    console.log(JSON.stringify(body))
     axios.post(`${BASE_URL}/postingRating`,body).then((response)=>{
         // alert("You have successfully booked the property");
         Swal.fire({
@@ -41,6 +88,11 @@ class UserCard extends Component {
             text: "Rating is saved!"
         }).then(()=>{
           console.log("reservation rating saved");
+
+          this.setState({
+            reviews : true
+          } );
+  
         });
       
     }).catch((error)=>{
@@ -65,6 +117,14 @@ class UserCard extends Component {
       .then((result) => {
         console.log("result"+result)
         alert("Your checkin is complete")
+
+        this.setState({
+          status : "Checkin complete"
+        } );
+
+        this.setState({
+          checkInState : true
+        } );
         //Grey out checkin
       })
       .catch(error =>
@@ -91,7 +151,15 @@ class UserCard extends Component {
       .then((result) => {
         console.log("result"+result)
         alert("Your checkout is complete")
-        //Grey out checkin
+
+        this.setState({
+          status : "Checkout complete"
+        } );
+
+        this.setState({
+          checkoutState : true
+        } );
+      
       })
       .catch(error =>
         { 
@@ -117,6 +185,14 @@ class UserCard extends Component {
       .then((result) => {
         console.log("result"+result)
         alert("Your reservation is cancelled")
+
+        this.setState({
+          status : "Reservation is cancelled"
+        } );
+
+        this.setState({
+          cancelled : true
+        } );
 
       })
       .catch(error =>
@@ -146,7 +222,8 @@ class UserCard extends Component {
 
     console.log("i m in rating")
 
-      if(true ) {
+      if(!this.state.reviews ) {
+        console.log("inside if")
         return (
           <div  style = { { left : "1000px", top : "10px"}}>
           <hr></hr>
@@ -181,6 +258,39 @@ class UserCard extends Component {
       );
        
       } 
+
+      else {
+        return (
+
+          <div  class= "row"   style = { { left : "100px", width : "1000px"}}>
+    
+          <h2> Rating complete     </h2>
+          
+          <div  class = "col-md-7">
+          <StarRatingComponent   
+          name="rating" 
+          starCount={5}
+          value={rating}
+
+          disabled 
+         
+        />
+
+       <strong> &nbsp;&nbsp;&nbsp;&nbsp; Review - </strong>
+
+        {trip.reservationReview}
+
+       
+        </div>
+
+
+     
+            
+     
+        </div>
+
+        );
+      }
     }
 
     render() { 
@@ -190,9 +300,6 @@ class UserCard extends Component {
         const enddate = new Date(trip.endDate).toLocaleDateString();
         
        console.log("***** Trip ****"+JSON.stringify(trip) )
-
-       
-       const status = (trip.isCancelled === 0)? "RESERVED" : "CANCELLED"
 
        //Change the URL
        const url = FRONTEND_URL+"/overview/"+trip.postingId
@@ -217,14 +324,14 @@ class UserCard extends Component {
                 <div className="col-md-7" >
                       <div className="row">
                           <div className="col-md-4">
-                            <button type="submit" className="btn btn-primary btn-block"  onClick={this.checkIn(trip.bookingId)}> Check In </button>
+                            <button type="submit" className="btn btn-primary btn-block" disabled= {this.state.checkInState}  onClick={this.checkIn(trip.bookingId)}> Check In </button>
                           </div>
                           <div className="col-md-4">
-                            <button type="submit" className="btn btn-primary btn-block"  onClick={this.checkOut(trip.bookingId)}> Check Out </button>
+                            <button type="submit" className="btn btn-primary btn-block"   disabled= {this.state.checkOutState}  onClick={this.checkOut(trip.bookingId)}> Check Out </button>
                         
                           </div>
                           <div className="col-md-4">
-                            <button type="submit" className="btn btn-primary btn-block" onClick={this.cancel(trip.bookingId)}  > Cancel Reservation </button>
+                            <button type="submit" className="btn btn-primary btn-block"   disabled= {this.state.cancelled}  onClick={this.cancel(trip.bookingId)}  > Cancel Reservation </button>
                           </div>
                       </div>
 
@@ -241,7 +348,7 @@ class UserCard extends Component {
             
 
             <div className="row">
-              <h3>Status {status} </h3>
+              <h3>Status {this.state.status} </h3>
               <hr></hr>
             </div>
 
