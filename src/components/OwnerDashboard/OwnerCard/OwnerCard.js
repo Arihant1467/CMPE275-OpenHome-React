d2634407b623d9e7ec503ae9216f99cf9295aea9
Arihant1467 <arihant95@gmail.com>
2019-12-17 20:27:14 -0800

    delete posting

--- src/components/OwnerDashboard/OwnerCard/OwnerCard.js
+++ src/components/OwnerDashboard/OwnerCard/OwnerCard.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Label, Input } from 'reactstrap';
 import './OwnerCard.css';
 import { BASE_URL } from './../../constants.js';
 import ImageGallery from './../../PropertyOverview/ImageGallery/ImageGallery.js';
@@ -11,18 +12,25 @@ class OwnerCard extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      editProperty:false,
+      editProperty: false,
     }
-    
+
   }
 
-  editProperty = (e)=>{
+  editPropertyBtn = (e) => {
     console.log("edit btn clicked");
-   const { editProperty } = this.state;
-   
-   this.setState({
-     editProperty: editProperty? false:true
-   });
+    const { editProperty } = this.state;
+
+    this.setState({
+      editProperty: editProperty ? false : true
+    });
+
+  }
+
+  deleteBtnClicked = (e) => {
+    console.log("delete btn clicked");
+    
+    this.props.onDeletePosting(this.props.data.propertyId);
 
   }
 
@@ -30,28 +38,31 @@ class OwnerCard extends Component {
     e.preventDefault();
     const form = serialize(e.target, { hash: true });
     const dayAvailability = generateDayAvailibility(form);
-    const {weekRent, weekendRent } = form;
-
+    const { weekRent, weekendRent } = form;
+    
     const body = {
       propertyId: this.props.data.propertyId,
-      startDate : new Date(form.startDate).getTime(),
-      endDate : new Date(form.endDate).getTime(),
+      startDate: new Date(form.startDate).getTime(),
+      endDate: new Date(form.endDate).getTime(),
       dayAvailability,
       weekRent,
-      weekendRent
+      weekendRent,
+      bearPenalty: form["bearPenalty"]==="1"
     };
 
+    console.log("Body to be posted for editing")
     console.log(body);
     
-    axios.put(`${BASE_URL}/postingAdd`,body).then((response)=>{
+    
+    axios.put(`${BASE_URL}/postingAdd`, body).then((response) => {
       console.log('response after editing property');
       console.log(response.data);
-      if(response.status===200){
-        this.props.onUpdateSuccess(response.data,this.props.data.propertyId);
+      if (response.status === 200) {
+        this.props.onUpdateSuccess(response.data, this.props.data.propertyId);
       }
-    }).catch((err)=>{
+    }).catch((err) => {
       console.log(err.data);
-      this.props.onUpdateError(err.data,this.props.data.propertyId);
+      this.props.onUpdateError(err.data, this.props.data.propertyId);
     })
   }
 
@@ -65,22 +76,22 @@ class OwnerCard extends Component {
       }
     }
 
-    const {startDate, endDate} = this.props.data;
-    const  {dayAvailability} = property;
-    const {weekendRent, weekRent} = this.props.data;
-    const {editProperty} = this.state;
-    const formStyle = {display: editProperty?'block':'none'};
+    const { startDate, endDate } = this.props.data;
+    const { dayAvailability } = property;
+    const { weekendRent, weekRent } = this.props.data;
+    const { editProperty } = this.state;
+    const formStyle = { display: editProperty ? 'block' : 'none' };
     console.log(`Edit property:${editProperty}`);
     console.log(`Day Availability ${dayAvailability}`);
 
     return (
 
       <div className="row justify-content-center row-style row-booking">
-        
+
         <div className="col-md-3">
           <ImageGallery photos={property.pictureUrl.split(";")} height="150px" />
         </div>
-        
+
         <div className="col-md-9">
           <table className="table">
             <thead>
@@ -108,99 +119,126 @@ class OwnerCard extends Component {
               </tr>
             </tbody>
           </table>
-            <button className="btn btn-primary btn-lg" onClick={this.editProperty}>Edit</button>
-            <form onSubmit={this.saveActionHandle} style={formStyle}>
-              <div className="row no-bg justify-content-center">
+          <button className="btn btn-primary btn-lg" onClick={this.editPropertyBtn}>{editProperty ? "Done":"Edit"}</button>
+          <button className="btn btn-danger btn-lg btn-save" onClick={this.deleteBtnClicked}>Delete Posting</button>
+          
+          <form onSubmit={this.saveActionHandle} style={formStyle}>
+            <div className="row no-bg justify-content-center">
 
+              
+              <div className="form-element" style={{display:'none'}}>
+                <div className="form-label">
+                  <label className="form-label">Start Date</label>
+                </div>
+                <div className="selector child-margin" style={{ marginTop: '20px' }}>
+                  <input type="date" className="form-control no-bg" name="startDate" defaultValue={new Date(startDate).toISOString().split('T')[0]} style={{ border: 'none', background: 'transparent', fontSize: '15px' }} required />
+                </div>
+              </div>
+
+              
               <div className="form-element">
-                        <div className="form-label">
-                            <label className="form-label">Start Date</label>
-                        </div>
-                        <div className="selector child-margin" style={{ marginTop: '20px' }}>
-                            <input type="date" className="form-control no-bg" name="startDate" defaultValue={new Date(startDate).toISOString().split('T')[0]}  style={{ border: 'none', background: 'transparent',fontSize:'15px' }} required />
-                        </div>
+                <div className="form-label">
+                  <label className="form-label">Bear 15% Penalty</label>
+                </div>
+                <div className="street-address child-margin">
+                  <input type="checkbox" name="bearPenalty" value="1" style={{ background: 'transparent' }} />
+                </div>
+              </div>
+
+
+              
+              <div className="form-element" style={{display:'none'}}>
+                <div className="form-label">
+                  <label className="form-label">End Date</label>
+                </div>
+                <div className="selector child-margin" style={{ marginTop: '20px' }}>
+                  <input type="date" className="form-control no-bg" name="endDate" defaultValue={new Date(endDate).toISOString().split('T')[0]} style={{ border: 'none', background: 'transparent', fontSize: '15px' }} required />
+                </div>
               </div>
 
               <div className="form-element">
-                        <div className="form-label">
-                            <label className="form-label">End Date</label>
-                        </div>
-                        <div className="selector child-margin" style={{ marginTop: '20px' }}>
-                            <input type="date" className="form-control no-bg"  name="endDate" defaultValue={new Date(endDate).toISOString().split('T')[0]}  style={{ border: 'none', background: 'transparent',fontSize:'15px' }} required />
-                        </div>
+                <div className="form-label">
+                  <label className="form-label">Weekday Rent</label>
+                </div>
+                <div className="street-address child-margin">
+                  <input type="number" defaultValue={weekRent} name="weekRent" placeholder="weekRent" style={{ background: 'transparent' }} />
+                </div>
               </div>
 
               <div className="form-element">
-                        <div className="form-label">
-                            <label className="form-label">Weekday Rent</label>
-                        </div>
-                        <div className="street-address child-margin">
-                            <input type="number" defaultValue={weekRent} name="weekRent" placeholder="weekRent" style={{ background: 'transparent' }} />
-                        </div>
-                    </div>
-
-                    <div className="form-element">
-                        <div className="form-label">
-                            <label className="form-label">Weekend Rent</label>
-                        </div>
-                        <div className="street-address child-margin">
-                            <input type="number" defaultValue={weekendRent} name="weekendRent" placeholder="Weekend Rent" style={{ background: 'transparent' }} />
-                        </div>
-                    </div>
+                <div className="form-label">
+                  <label className="form-label">Weekend Rent</label>
+                </div>
+                <div className="street-address child-margin">
+                  <input type="number" defaultValue={weekendRent} name="weekendRent" placeholder="Weekend Rent" style={{ background: 'transparent' }} />
+                </div>
+              </div>
 
               <div className="form-element">
-                            <div className="form-label">
-                                <label className="form-label">Days Availibility</label>
-                            </div>
-                            <div className="selector" style={{ margin: '20px 0px 0px 3px' }}>
+                <div className="form-label">
+                  <label className="form-label">Days Availibility</label>
+                </div>
+                <div className="selector" style={{ margin: '13px 0px 0px 3px' }}>
 
-                                <div className="form-check form-check-inline">
-                                    <input className="form-check-input" type="checkbox" defaultChecked={dayAvailability[0]==="1"}  name="sun" value="1" />
-                                    <label className="form-check-label" htmlFor="sun">Sunday</label>
+                  
+                <div className="form-check-inline">
+                  <Label className="form-check-label" ><Input className="form-check-input" type="checkbox" defaultChecked={dayAvailability[0] === "1"} name="sun" value="1" />Sunday</Label>
+                  </div>
 
-                                </div>
+                  <div className="form-check-inline">
+                  <Label className="form-check-label" check><Input className="form-check-input" type="checkbox" defaultChecked={dayAvailability[1] === "1"} name="mon" value="1" />Monday</Label>
+                  </div>
 
-                                <div className="form-check form-check-inline">
-                                    <input className="form-check-input" type="checkbox" defaultChecked={dayAvailability[1]==="1"}  name="mon" value="1" />
-                                    <label className="form-check-label" htmlFor="mon">Monday</label>
-                                </div>
+                  
+                  <div className="form-check-inline">
+                  <Label className="form-check-label" check><Input className="form-check-input" type="checkbox" defaultChecked={dayAvailability[2] === "1"} name="tue" value="1" />Tuesday</Label>
+                    
+                  </div>
 
-                                <div className="form-check form-check-inline">
-                                    <input className="form-check-input" type="checkbox" defaultChecked={dayAvailability[2]==="1"}  name="tue" value="1" />
-                                    <label className="form-check-label" htmlFor="tue">Tuesday</label>
-                                </div>
+                  <div className="form-check-inline">
+                  <Label className="form-check-label" check><Input className="form-check-input" type="checkbox" defaultChecked={dayAvailability[3] === "1"} name="wed" value="1" />Wednesday</Label>
+                    
+                  </div>
 
-                                <div className="form-check form-check-inline">
-                                    <input className="form-check-input" type="checkbox" defaultChecked={dayAvailability[3]==="1"}  name="wed" value="1" />
-                                    <label className="form-check-label" htmlFor="wed">Wednesday</label>
-                                </div>
+                  {/* <div className="form-check form-check-inline">
+                    <input className="form-check-input" type="checkbox" defaultChecked={dayAvailability[4] === "1"} name="thur" value="1" />
+                    <label className="form-check-label" htmlFor="thur">Thursday</label>
+                  </div> */}
+                  <div className="form-check-inline">
+                  <Label className="form-check-label" check><Input className="form-check-input" type="checkbox" defaultChecked={dayAvailability[4] === "1"} name="thur" value="1" />Thursday</Label>
+                    
+                  </div>
 
-                                <div className="form-check form-check-inline">
-                                    <input className="form-check-input" type="checkbox" defaultChecked={dayAvailability[4]==="1"}  name="thur" value="1" />
-                                    <label className="form-check-label" htmlFor="thur">Thursday</label>
-                                </div>
+                  {/* <div className="form-check form-check-inline">
+                    <input className="form-check-input" type="checkbox" defaultChecked={dayAvailability[5] === "1"} name="fri" value="1" />
+                    <label className="form-check-label" htmlFor="fri">Friday</label>
+                  </div> */}
+                  <div className="form-check-inline">
+                  <Label className="form-check-label" check><Input className="form-check-input" type="checkbox" defaultChecked={dayAvailability[5] === "1"} name="fri" value="1" />Friday</Label>
+                    
+                  </div>
 
-                                <div className="form-check form-check-inline">
-                                    <input className="form-check-input" type="checkbox" defaultChecked={dayAvailability[5]==="1"}  name="fri" value="1" />
-                                    <label className="form-check-label" htmlFor="fri">Friday</label>
-                                </div>
+                  {/* <div className="form-check form-check-inline">
+                    <input className="form-check-input" type="checkbox" defaultChecked={dayAvailability[6] === "1"} name="sat" value="1" />
+                    <label className="form-check-label" htmlFor="sat">Saturday</label>
+                  </div> */}
+                  <div className="form-check-inline">
+                  <Label className="form-check-label" check><Input className="form-check-input" type="checkbox" defaultChecked={dayAvailability[6] === "1"} name="sat" value="1" />Saturday</Label>
+                    
+                  </div>
 
-                                <div className="form-check form-check-inline">
-                                    <input className="form-check-input" type="checkbox" defaultChecked={dayAvailability[6]==="1"}  name="sat" value="1" />
-                                    <label className="form-check-label" htmlFor="sat">Saturday</label>
-                                </div>
 
-
-                            </div>
-                        </div>
-
-                        
-                        <div className="col-md-2">
-                            <button type="submit" className="btn btn-primary btn-block btn-save">Save</button>
-                        </div>
-
+                </div>
+                
               </div>
-            </form>
+
+              
+              <div className="col-md-1" style={{marginTop:'5px',marginBottom:'5px'}}>
+                <button type="submit" className="btn btn-primary btn-block btn-save">Save</button>
+              </div>
+
+            </div>
+          </form>
         </div>
       </div>
 
